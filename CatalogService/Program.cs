fd61f8a72f109624bfd31e2971cb5f483a2a3cb9
Lennart Kampshoff <lennart@kampshoff.xyz>
2022-06-04 14:20:28 +0200

    Add project files.

---
+++ CatalogService/Program.cs
@@ -0,0 +1,30 @@
+using Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClients(builder.Configuration.GetSection("Services"));
+builder.Logging.AddSeq(builder.Configuration["Seq"]);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Shared/ServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddDatabaseContext<TContext>(this IServiceCollection services, string connectionString) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options =>
+            {
+                options.UseNpgsql(connectionString, npgsqlOptions =>
+                {
+                    npgsqlOptions.EnableRetryOnFailure();
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration serviceConfiguration)
+        {
+            services.AddHttpClient("cart", client =>
+            {
+                client.BaseAddress = new Uri(serviceConfiguration["Cart"]);
+            });
+
+            services.AddHttpClient("shipping", client =>
+            {
+                client.BaseAddress = new Uri(serviceConfiguration["Shipping"]);
+            });
+
+            services.AddHttpClient("checkout", client =>
+            {
+                client.BaseAddress = new Uri(serviceConfiguration["Checkout"]);
+            });
+
+            services.AddHttpClient("catalog", client =>
+            {
+                client.BaseAddress = new Uri(serviceConfiguration["Catalog"]);
+            });
+
+            return services;
+        }
+    }
+}
