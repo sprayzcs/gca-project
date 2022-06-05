c824346250ecb2c891e66e520d69dd216de87e3b
Philipp <philipphonsel@gmail.com>
2022-06-05 13:55:32 +0200

    Add Product Card

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,26 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NbActionsModule, NbIconModule, NbLayoutModule, NbTagModule, NbThemeModule } from '@nebular/theme';
+import { NbActionsModule, NbButtonModule, NbCardModule, NbIconModule, NbLayoutModule, NbTagModule, NbThemeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { CartComponent } from './routes/cart/cart.component';
 import { HomeComponent } from './routes/home/home.component';
+import { ProductCardComponent } from './routes/home/components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NbThemeModule.forRoot(),
+    NbThemeModule.forRoot({name: 'light'}),
     NbEvaIconsModule,
     NbLayoutModule,
     NbActionsModule,
     NbTagModule,
     NbIconModule,
+    NbCardModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
