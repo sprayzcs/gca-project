f20431fb29e3d1e689c86f8e40326c49dd048f48
Lennart Kampshoff <lennart@kampshoff.xyz>
2022-06-09 15:41:07 +0200

    ✨ Add Dto models

---
+++ Shared/Data/OrderDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Data;
+
+public class OrderDto
+{
+    public Guid Id { get; set; }
+
+    public DateTimeOffset Date { get; set; }
+
+    public string Country { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string Zipcode { get; set; } = string.Empty;
+
+    public string Street { get; set; } = string.Empty;
+
+    public string CreditCardNumber { get; set; } = string.Empty;
+
+    public DateOnly CreditCardExpiryDate { get; set; }
+
+    public Guid CardId { get; set; }
+
+    public Guid ShipmentId { get; set; }
+}
---
+++ Shared/Data/ShipmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Data;
+
+public class ShipmentDto
+{
+    public Guid Id { get; set; }
+
+    public string TrackingNumber { get; set; } = string.Empty;
+}
