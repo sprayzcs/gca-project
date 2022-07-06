34fcf66aef5ed4b250118ab94051a743856f9a51
Lennart Kampshoff <33363043+sprayzcs@users.noreply.github.com>
2022-07-06 22:19:32 +0200

    📝 Fix catalog service swagger response types 🍻

--- Backend/Services/CatalogService/Controllers/CatalogController.cs
+++ Backend/Services/CatalogService/Controllers/CatalogController.cs
@@ -18,13 +18,14 @@ public class CatalogController : BaseController
     }
 
     [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ProductDto>))]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel<IEnumerable<ProductDto>>))]
     public async Task<IActionResult> GetItems()
     {
         return Result(await _service.GetProducts());
     }
 
     [HttpGet("/list")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel<IEnumerable<ProductDto>>))]
     public async Task<IActionResult> GetItemsByIds([FromQuery] IEnumerable<Guid> productIds, CancellationToken cancellationToken)
     {
         return Result(await _service.GetProductsByIdsAsync(productIds, cancellationToken));
@@ -32,7 +33,8 @@ public class CatalogController : BaseController
 
     [HttpGet]
     [Route("/{id:guid}")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProductDto))]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel<ProductDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
     public async Task<IActionResult> GetItem([FromRoute] Guid id)
     {
         return Result(await _service.GetProduct(id));
