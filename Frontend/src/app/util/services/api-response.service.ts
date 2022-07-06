43a09135701a37265684b70333872a4e1d3e29ec
Philipp <philipphonsel@gmail.com>
2022-07-06 23:09:12 +0200

    🧑‍💻 Update apiresponseservice to be more generic

--- Frontend/src/app/util/services/api-response.service.ts
+++ Frontend/src/app/util/services/api-response.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { NbGlobalPhysicalPosition, NbToastrService } from "@nebular/theme";
 import { Store } from "@ngxs/store";
-import { catchError, map, Observable, throwError } from "rxjs";
+import { catchError, map, Observable, Operator, OperatorFunction, throwError } from "rxjs";
 import { FailAction } from "src/app/store/actions/base.actions";
 import { ApiResponseModel } from "../models/api-response.model";
 
@@ -17,39 +17,19 @@ export class ApiResponseService {
         private readonly store: Store
     ) { }
 
-    public resolveGet<T>(
+    public resolveGet<TResponse>(
         system: string,
         url: string,
-        onFinish: Function,
-        failAction?: (errors: string[]) => FailAction,
-        showErrors: boolean = true): Observable<T | undefined> {
+        onFinish: () => void,
+        failAction: (errors: string[]) => FailAction,
+        showErrors: boolean = true): Observable<TResponse | undefined> {
         if(!url.startsWith('/')){
             url = `/${url}`;
         }
 
-        return this.http.get<ApiResponseModel<T>>(system + url)
+        return this.http.get<ApiResponseModel<TResponse>>(system + url)
             .pipe(
-                catchError(error => {
-                    if(showErrors){
-                        this.showErrorToast(error.error.error);
-                    }
-                    this.store.dispatch(failAction);
-                    onFinish();
-                    return throwError(() => new Error(`Get request to ${system + url} failed.`));
-                }),
-                map(response => {
-                    if(!response.success){
-                        if(showErrors){
-                            this.showErrorToast(response.errors);
-                        }
-                        this.store.dispatch(failAction);
-                        onFinish();
-                        return undefined;
-                    }
-
-                    onFinish();
-                    return response.data;
-                })
+                ...this.handle<TResponse>(showErrors, onFinish, failAction),
             );
     }
 
@@ -57,8 +37,8 @@ export class ApiResponseService {
         system: string,
         url: string,
         data: TRequest,
-        onFinish: Function,
-        failAction?: (errors: string[]) => FailAction,
+        onFinish: () => void,
+        failAction: (errors: string[]) => FailAction,
         showErrors: boolean = true): Observable<TResponse | undefined> {
         if(!url.startsWith('/')){
             url = `/${url}`;
@@ -66,37 +46,65 @@ export class ApiResponseService {
 
         return this.http.post<ApiResponseModel<TResponse>>(system + url, data)
             .pipe(
-                catchError(error => {
-                    if(showErrors){
-                        this.showErrorToast(error?.error?.error ?? 'Es ist ein Fehler aufgetreten');
-                    }
-                    this.store.dispatch(failAction);
-                    onFinish();
-                    return throwError(() => new Error(`Get request to ${system + url} failed.`));
-                }),
-                map(response => {
-                    if(!response.success){
-                        if(showErrors){
-                            this.showErrorToast(response.errors);
-                        }
-                        this.store.dispatch(failAction);
-                        onFinish();
-                        return undefined;
-                    }
-
-                    onFinish();
-                    return response.data;
-                })
+                ...this.handle<TResponse>(showErrors, onFinish, failAction),
             );
     }
 
+    public resolvePatch<TRequest, TResponse>(
+        system: string,
+        url: string,
+        data: TRequest,
+        onFinish: () => void,
+        failAction: (errors: string[]) => FailAction,
+        showErrors: boolean = true): Observable<TResponse | undefined> {
+        if(!url.startsWith('/')){
+            url = `/${url}`;
+        }
+
+        return this.http.patch<ApiResponseModel<TResponse>>(system + url, data)
+            .pipe(
+                ...this.handle<TResponse>(showErrors, onFinish, failAction),
+            );
+    }
+
+    private handle<TResponse>(showErrors: boolean, onFinish: () => void, failAction: (errors: string[]) => FailAction)
+        : [OperatorFunction<any, any>, OperatorFunction<ApiResponseModel<TResponse>, TResponse | undefined>] {
+        return [
+            catchError(error => {
+                if(showErrors){
+                    this.showErrorToast(error?.error?.error ?? 'Es ist ein Fehler aufgetreten');
+                }
+                this.store.dispatch(failAction(error?.error?.error ?? []));
+                onFinish();
+                return throwError(() => new Error(`Get request failed.`));
+            }),
+            map((response: ApiResponseModel<TResponse>) => {
+                if(!response.success){
+                    if(showErrors){
+                        this.showErrorToast(response.errors);
+                    }
+                    this.store.dispatch(failAction(response.errors ?? []));
+                    onFinish();
+                    return undefined;
+                }
+
+                onFinish();
+                return response.data;
+            })
+        ];
+    }
+
     private showErrorToast(errorMessages: string[] | undefined = undefined): void {
-        this.toastrService.danger(errorMessages?.join(', ') ?? '', 'Es ist ein Fehler aufgetreten', {
-            duration: 5,
-            hasIcon: true,
-            icon: 'alert-circle-outline',
-            position: NbGlobalPhysicalPosition.BOTTOM_RIGHT
-        });
+        this.toastrService.danger(
+            errorMessages?.join(', ') ?? '',
+            'Es ist ein Fehler aufgetreten', 
+            {
+                duration: 5000,
+                hasIcon: true,
+                icon: 'alert-circle-outline',
+                position: NbGlobalPhysicalPosition.BOTTOM_RIGHT
+            }
+        );
     }
 
 }
