ad35e41cf07a3debd27a91177ab3f982971ca14e
Lennart Kampshoff <33363043+sprayzcs@users.noreply.github.com>
2022-07-06 15:47:44 +0200

    🗃️ Limit length of some text fields

--- Backend/Services/CheckoutService/Data/Address.cs
+++ Backend/Services/CheckoutService/Data/Address.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheckoutService.Data;
 
 [Owned]
 public class Address
 {
+    [MaxLength(200)]
     public string Country { get; set; } = string.Empty;
 
+    [MaxLength(200)]
     public string City { get; set; } = string.Empty;
 
+    [MaxLength(200)]
     public string Zipcode { get; set; } = string.Empty;
 
+    [MaxLength(200)]
     public string Street { get; set; } = string.Empty;
 }
--- Backend/Services/CheckoutService/Data/PaymentInfo.cs
+++ Backend/Services/CheckoutService/Data/PaymentInfo.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheckoutService.Data;
 
 [Owned]
 public class PaymentInfo
 {
+    [MaxLength(16)]
     public string CreditCardNumber { get; set; } = string.Empty;
 
     public DateTimeOffset CreditCardExpiryDate { get; set; }
