5bedd71ab8f80bddcfb7d207d846411cd5c8d119
Philipp <philipphonsel@gmail.com>
2022-07-10 14:52:54 +0200

    🧑‍💻 Generalize swagger with xml by extension method

--- Backend/Services/ShippingService/Program.cs
+++ Backend/Services/ShippingService/Program.cs
@@ -13,12 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddSwaggerWithXML(Assembly.GetExecutingAssembly().GetName().Name);
 
 builder.Services.AddNotificationHandler();
 builder.Services.AddDatabaseContext<ShipmentContext>(builder.Configuration["ConnectionString"]);
