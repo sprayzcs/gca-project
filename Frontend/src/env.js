7d00997ac25620000e038c7b97f144c465f755da
Philipp <philipphonsel@gmail.com>
2022-07-14 18:40:57 +0200

    🐛  Fix build

---
+++ Frontend/src/env.js
@@ -0,0 +1,8 @@
+
+(function (window) {
+    window["env"] = window["env"] || {};
+    window["env"].CATALOG_URL = 'http://localhost:5001'; // not actualized, for local testing
+    window["env"].SHIPPING_URL = 'http://localhost:6001'; // not actualized, for local testing
+    window["env"].CART_URL = 'http://localhost:7001'; // not actualized, for local testing
+    window["env"].CHECKOUT_URL = 'http://localhost:8001'; // not actualized, for local testing
+  })(this);
